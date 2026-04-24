b90407a0e5e2e65f4977795e49fdb25491430837
ari <souza.santos.arielly@escola.pr.gov.br>
2026-04-24 14:54:06 -0300

    salvar

---
+++ script/script.js
@@ -0,0 +1,35 @@
+function salvarCarros(event) {
+    //impedir de recarregar a página
+    event.preventDefault();
+
+    let titulo = document.getElementById('title');
+    let preco = document.getElementById('preco');
+    let marca = document.getElementById('marca');
+    let modelo = document.getElementById('modelo');
+
+    let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
+
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
+
+    let lista = document.getElementById('listarCarros');
+
+    let card = document.createElement('div');
+
+    card.style.border = "1px solid #ccc";
+    card.style.padding = "10px";
+    card.style.marginBottom = "10px";
+
+    lista.appendChild(card);
+
+    let carro = {
+        titulo,
+        preco,
+        marca,
+        modelo, 
+        cambio
+    };
+
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON.stringify(carros));
+}
